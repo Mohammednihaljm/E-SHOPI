a2122964359a78f23ca78419986b63e004dd335f
Mohammednihaljm <mohammednihaljm@gmail.com>
2023-11-22 12:51:31 +0530

    useReducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import {BrowserRouter,  Route, Routes } from 'react-router-dom'
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+// import HomeScreenTest from "./screens/HomeScreenTest";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/product/:slug" element={<ProductScreen/>}/>
           <Route path="/" element={<HomeScreen/>}/>
+          {/* <Route path="/" element={<HomeScreenTest/>}/> */}
         </Routes>
         
       </main>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer,} from 'react'
 import axios from 'axios'
+import logger from 'use-reducer-logger'
 // import data from '../data'
 
+const reducer= (state,action)=>{
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return {...state,loading:true};
+    case 'FETCH_SUCCESS':
+      return {...state,products:action.payload,loading:false};  
+    case 'FETCH_Fail':
+      return {...state,loading:false,error:action.payload};
+    default:
+      return state;
+  }
+
+}
+
 const HomeScreen = () => {
+
+  const [{loading,error,products},dispatch]=useReducer(logger(reducer),{
+ 
+    products:[],
+    loading:true,
+    error:'',
+  })
    
-  const [products, setProducts] = useState([])
+  // const [products, setProducts] = useState([])
 
   useEffect(()=>{
 
     const fetchData=async()=>{
+      dispatch({type:'FETCH_REQUEST'})
+      try{
+        const result= await axios.get('/api/products')
+        dispatch({type:'FETCH_SUCCESS',payload:result.data})
 
-      const result= await axios.get('/api/products')
-      setProducts(result.data)
+      }catch(err){
+        dispatch({type:'FETCH_Fail',payload:err.message})
+      }
+      // setProducts(result.data)
 
     }
     fetchData()
@@ -24,7 +52,12 @@ const HomeScreen = () => {
         <h1>feature products</h1>
         
         <div className="products">
-          {products.map((product) => (
+
+          {
+            loading ? (<div>Loading...</div>):error ? (<div>
+              {error}
+            </div>):(
+          products.map((product) => (
             <div className="product" key={product.slug}>
               <a href={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
@@ -38,7 +71,7 @@ const HomeScreen = () => {
               <button>Add product</button>
               </div>
             </div>
-          ))}
+          )))}
         </div>
     </div>
   )
