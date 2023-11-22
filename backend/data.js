46151214b3a53b6c956a82644e59dab7d441dfd8
Mohammednihaljm <mohammednihaljm@gmail.com>
2023-11-22 18:29:08 +0530

    bootstrap

--- backend/data.js
+++ backend/data.js
@@ -35,6 +35,39 @@ const data={
     rating:4.5,
     numReviews:10,
     description:"high qualty shirt"
+},{
+    name:"NIKE SLIM SHIRT",
+    slug: "addidad-slim-shirt",
+    category:"shirts",
+    image:"/images/P4.jpg",
+    price:120,
+    countInStock:"10",
+    brand:"Nike",
+    rating:4.5,
+    numReviews:10,
+    description:"high qualty shirt"
+},{
+    name:"NIKE SLIM SHIRT",
+    slug: "Nike-slim-shirt",
+    category:"shirts",
+    image:"/images/p1.jpg",
+    price:120,
+    countInStock:"10",
+    brand:"Nike",
+    rating:4.5,
+    numReviews:10,
+    description:"high qualty shirt"
+},{
+    name:"NIKE SLIM SHIRT",
+    slug: "addidad-slim-shirt",
+    category:"shirts",
+    image:"/images/P4.jpg",
+    price:120,
+    countInStock:"10",
+    brand:"Nike",
+    rating:4.5,
+    numReviews:10,
+    description:"high qualty shirt"
 }]
 }
 export default data
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -35,6 +35,17 @@ const data={
     rating:4.5,
     numReviews:10,
     description:"high qualty shirt"
+},{
+    name:"NIKE SLIM SHIRT",
+    slug: "addidad-slim-shirt",
+    category:"shirts",
+    image:"/images/p3.jpg",
+    price:120,
+    countInStock:"10",
+    brand:"Nike",
+    rating:4.5,
+    numReviews:10,
+    description:"high qualty shirt"
 }]
 }
 export default data
